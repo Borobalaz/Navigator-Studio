48531b373eb250532b42c1a2872e9ee34942ac4b
Borobalaz <boroczky.balazs@gmail.com>
2026-02-16 00:44:32 +0100

    release: bump version

--- release.cjs
+++ release.cjs
@@ -5,7 +5,7 @@ const { execSync } = require('child_process');
 
 // bump version, push, and publish
 execSync('git add .', { stdio: 'inherit' });
-execSync('git commit', { stdio: 'inherit' });
+execSync('git commit -m "release: bump version"', { stdio: 'inherit' });
 execSync('npm version patch', { stdio: 'inherit' });
 execSync('git push', { stdio: 'inherit' });
 execSync('npx electron-builder --publish always', { stdio: 'inherit' });
