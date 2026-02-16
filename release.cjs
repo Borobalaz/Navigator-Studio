91140c17f51cf9861ba191109c19e0bb823ea00b
Böröczky Balázs <boroczky.balazs@gmail.com>
2026-02-16 14:40:11 +0100

    ok

--- release.cjs
+++ release.cjs
@@ -6,4 +6,6 @@ const { execSync } = require('child_process');
 // bump version, push, and publish
 execSync('npm version patch', { stdio: 'inherit' });
 execSync('git push', { stdio: 'inherit' });
-execSync('npx electron-builder --publish always', { stdio: 'inherit' });
+execSync('npx electron-builder --publish always', {
+  stdio: 'inherit', env: process.env
+});
