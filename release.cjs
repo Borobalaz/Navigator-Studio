ac78515e7ea2a175f5d9adc06ec40c576e8f7def
Borobalaz <boroczky.balazs@gmail.com>
2026-02-16 01:16:30 +0100

    erm

--- release.cjs
+++ release.cjs
@@ -4,8 +4,6 @@ require('dotenv').config(); // load .env
 const { execSync } = require('child_process');
 
 // bump version, push, and publish
-execSync('git add .', { stdio: 'inherit' });
-execSync('git commit -m "release: bump version"', { stdio: 'inherit' });
 execSync('npm version patch', { stdio: 'inherit' });
 execSync('git push', { stdio: 'inherit' });
 execSync('npx electron-builder --publish always', { stdio: 'inherit' });
